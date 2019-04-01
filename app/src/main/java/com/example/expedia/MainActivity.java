d80403e065095e278983d1de503f7565b6d9f481
Moonsyn <msy9427@gmail.com>
2019-04-02 01:43:23 +0900

    4.2

---
+++ app/src/main/java/com/example/expedia/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.expedia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button hotelReservebtn;
+    private Button planeReservebtn;
+    private Button hotelAndPlaneReservebtn;
+    private ImageButton hotelSalebtn;
+    private ImageButton dailySalebtn;
+    private ImageButton deadlineSalebtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hotelReservebtn = findViewById(R.id.hotel_reserve_btn);
+        planeReservebtn = findViewById(R.id.plane_reserve_btn);
+        hotelAndPlaneReservebtn = findViewById(R.id.hotel_plane_reserve_btn);
+        hotelSalebtn = findViewById(R.id.hotel_sale_btn);
+        dailySalebtn = findViewById(R.id.daily_sale_btn);
+        deadlineSalebtn = findViewById(R.id.deadline_sale_btn);
+    }
+
+}
---
+++ app/src/main/java/com/example/expedia/StartActivity.java
@@ -0,0 +1,34 @@
+package com.example.expedia;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button next = findViewById(R.id.nextButton);
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
